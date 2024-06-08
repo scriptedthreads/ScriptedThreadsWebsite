f7304fc771c38f60c546c5235fadce639ac74c2c
faraz-github <farazahmad.email@gmail.com>
2024-06-09 02:17:01 +0530

    Add get started modal

--- scripts/main.js
+++ scripts/main.js
@@ -23,3 +23,29 @@ brandLogoImg.addEventListener("click", function () {
 let copyrightYear = document.querySelector("#copyright-year");
 
 copyrightYear.appendChild(document.createTextNode(new Date().getFullYear()));
+
+// Open And Close Get Started Modal
+var getStartedModal = document.getElementById("get-started-modal");
+
+var getStartedButton = document.getElementById("get-started-btn");
+var getStartedButtonMobile = document.getElementById("get-started-btn-mobile");
+
+var modalCloseX = document.querySelector(".modal-close-x");
+
+getStartedButton.onclick = function () {
+  getStartedModal.style.display = "block";
+};
+
+getStartedButtonMobile.onclick = function () {
+  getStartedModal.style.display = "block";
+};
+
+modalCloseX.onclick = function () {
+  getStartedModal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == getStartedModal) {
+    getStartedModal.style.display = "none";
+  }
+};
